98218a0fd70c010d0232f84f09caecf64dce13e5
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-06-05 16:40:04 -0600

    disco timegit add disco.js

--- disco.js
+++ disco.js
@@ -1,11 +1,39 @@
 const axios = require('axios')
 
-function yellow(red, yellow, blue) {
- axios({
-   url: 'http://10.9.21.211:80/api/led/change',
-   method: 'post',
-   data: { red: 255, green: 0, blue: 0 }
- })
+function partyTime(red, yellow, blue) {
+  axios({
+    url: 'http://10.9.21.211:80/api/audio/play',
+    method: 'post',
+    data: {
+      AssetId: "MrMeeseeks.wav"
+    }
+  })
+
+  axios({
+    url: 'http://10.9.21.211:80/api/images/change',
+    method: 'post',
+    data: {
+      FileName: "mrmee.jpg"
+    }
+  })
+
+  axios({
+    url: 'http://10.9.21.211:80/api/drive/time',
+    method: 'post',
+    data: {
+      LinearVelocity: 0,
+      AngularVelocity: 100,
+      TimeMS: 5000
+    }
+  })
+  .then(()=>{
+    axios({
+      url: 'http://10.9.21.211:80/api/images/revert',
+      method: 'post'
+    })
+  })
+
+
 }
 
-yellow()
+partyTime()
