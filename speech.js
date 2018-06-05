430c20d76345b2ef8099bfc38c4dd2ca373630a1
Kelly L <lock2846@gmail.com>
2018-06-05 15:34:42 -0600

    Second commit

---
+++ speech.js
@@ -0,0 +1,11 @@
+// const axios = require('axios')
+//
+// function yellow(red, yellow, blue) {
+//  axios({
+//    url: 'http://10.9.21.211:80/api/led/change',
+//    method: 'post',
+//    data: { red: 255, green: 0, blue: 0 }
+//  })
+// }
+//
+// yellow()
