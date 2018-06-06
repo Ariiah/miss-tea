c4e91f62623e7f9cbaa48a389fd58c1586914cb7
Kelly L <lock2846@gmail.com>
2018-06-06 15:19:53 -0600

    removed old code

--- speechscript.js
+++ speechscript.js
@@ -1,26 +1,5 @@
 const axios = require('axios')
-// const fs = require('fs')
-// const FileReader = require('filereader')
 require('dotenv').config();
-
-// WATSON NODE
-// var TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
-// var fs = require('fs');
-//
-// var textToSpeech = new TextToSpeechV1({
-//   username: process.env.USERNAME,
-//   password: process.env.PASSWORD
-// })
-// var synthesizeParams = {
-//   text: 'help me',
-//   accept: 'audio/mp3',
-//   voice: 'en-US_AllisonVoice'
-// }
-//
-//  Pipe the synthesized text to a file.
-// textToSpeech.synthesize(synthesizeParams).on('error', function(error) {
-//   console.log(error);
-// }).pipe(fs.createWriteStream('speech.mp3'))
 
 const TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
 const fs = require('fs')
@@ -41,51 +20,9 @@ textToSpeech.synthesize(synthesizeParams).on('error', function(error) {
   console.log(error);
 }).pipe(fs.createWriteStream('disguise.mp3'))
 
-// CURL
-// curl - X POST - u 041c85e0-e0d7-4cce-b093-3bcf4347d84c:rS7qqVaNHB1Z
-// --header "Content-Type: application/json"
-// --header "Accept: audio/mp3"
-// --data "{\"text\":\"What is my purpose?\"}"
-// --output purpose.mp3 "https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize"
+// BYTE ARRAY
 
-// AXIOS
-// axios({
-//   method: 'post',
-//   url: 'https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Accept': 'audio/mp3',
-//     'Authorization': `Basic ${process.env.USERNAME}:${process.env.PASSWORD}`
-//   },
-//   data: {
-//     'text': 'testing 1 2 3'
-//   }
-// }).then(result => {
-//   console.log(' i am super successful in life')
-//   // console.log(result);
-// }).catch(err => {
-//   // console.log(err);
-//   console.log("i did not succeed in life", err)
-// })
 
-// axios.post('https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize',{
-//   method: 'post',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Accept': 'audio/mp3',
-//   },
-//   auth: {
-//     username: process.env.USERNAME,
-//     password: process.env.PASSWORD
-//   },
-//   data: {
-//     text: 'testing 1 2 3'
-//   }
-//   }).then(result => {
-//     console.log(' i am super successful in life')
-//   }).catch(err => {
-//     console.log("i did not succeed in life", err)
-//   })
 
 // MISTY CALLS
 
